8ab246461795fef9333269262f4931da6e8e0829
Donnie D'Amato <fauxserious@gmail.com>
2020-08-06 10:08:31 -0400

    Working sticky

--- components/sticky-notes/sticky-notes.js
+++ components/sticky-notes/sticky-notes.js
@@ -9,104 +9,50 @@ export default class StickyNotes extends HTMLElement {
     this._noteContainer = this.shadowRoot.querySelector('.sticky-notes');
   }
 
-  static get observedAttributes() {
-    return ['amount', 'image'];
-  }
-
-  attributeChangedCallback(attrName, oldVal, newVal) {
-    if (attrName === 'amount') {
-      this._render();
-    }
-  }
-
-  get amount() {
-    return Number(this.getAttribute('amount'));
-  }
-
-  set amount(newVal) {
-    this.setAttribute('amount', Number(newVal));
-  }
-
-  get image() {
-    return this.getAttribute('image');
-  }
-
-  set image(newVal) {
-    if (newVal) {
-      this.setAttribute('image', newVal);
-    } else {
-      this.removeAttribute('image');
-    }
+  connectedCallback() {
+    this._render();
   }
 
   _render() {
     this._noteContainer.innerHTML = '';
-    this._notes = Array(this.amount).fill(0).map(() => {
+    this._notes = Array(8).fill(0).map(() => {
       return this._noteContainer.appendChild(this._createNote())
     });
+    window.setTimeout(() => {this._placeImages()}, 0);
   }
 
   _createNote() {
     const note = document.createElement('div');
     note.classList.add('sticky-note');
+    const tiltMax = 5;
+    const tiltMin = -5;
+    const tilt = Math.floor(Math.random() * (tiltMax - tiltMin + 1) + tiltMin);
+    note.style.setProperty('--rotatez', tilt + 'deg');
     return note;
   }
 
-  connectedCallback() {
-    this._observer = new ResizeObserver((entries) => {
-      window.requestAnimationFrame(() => {
-        if (!Array.isArray(entries) || !entries.length) {
-          return;
-        }
-        this._checkAlignment();
-      });
-    });
-    this._observer.observe(this._noteContainer);
-  }
-
-  disconnectedCallback() {
-    this._observer.disconnect();
-  }
-
-  _checkAlignment() {
-    const topAlignment = this._notes.filter((note) => {
-      const offsetMatch = note.offsetTop === this._notes[0].offsetTop;
-      const visible = note.classList.contains('hidden');
-      return offsetMatch;
-    }).length;
-    if (!this._topAlignment || this._topAlignment !== topAlignment) {
-      this._topAlignment = topAlignment;
-      this._placeImages();
-    }
-  }
-
   _placeImages() {
-    const colAmount = this._topAlignment;
-
-    const rowAmount = Math.ceil(this.amount / colAmount);
-    const noteHeight = this._notes[0].offsetHeight;
-    const noteWidth = this._notes[0].offsetHeight;
-    const threshold = Math.ceil(colAmount / rowAmount * colAmount * noteWidth / noteHeight) * noteHeight;
-    console.log(threshold);
-
-    const xPercent = 100 / (colAmount - 1);
-
-    const singleCol = colAmount === 1;
+    const colAmount = Math.floor(this._noteContainer.offsetWidth / this._notes[0].offsetWidth);
+    const rowAmount = Math.ceil(this._notes.length / colAmount);
+    
+    const xPercent = 100 / colAmount;
+    const yPercent = 100 / rowAmount;
 
     this._notes.forEach((note, i) => {
       const x = i % colAmount;
       const y = Math.floor(i / colAmount);
-      const yOffset = y * noteHeight;
-      
-      if (yOffset > threshold || (singleCol && i > 0)) {
-        note.classList.add('hidden');
-        note.style.setProperty('background-image', 'none');
-      } else {
-        note.classList.remove('hidden');
-        note.style.setProperty('background-image', `url(${this.image})`);
-        note.style.setProperty('background-position', `${x * xPercent}% -${yOffset}px`);
-        note.style.setProperty('background-size', `${colAmount * 100}%`);
-      }
+
+      const t = y * yPercent;
+      const b = 100 - (t + yPercent);
+      const l = x * xPercent;
+      const r = 100 - (l + xPercent);
+      note.style.setProperty('--origin-left', l + '%');
+      note.style.setProperty('--origin-top', t + '%');
+      note.style.setProperty('--origin-right', r + '%');
+      note.style.setProperty('--origin-bottom', b + '%');
+      note.style.setProperty('--origin-width', xPercent + '%');
+      note.style.setProperty('--origin-height', yPercent + '%');
+      note.style.setProperty('--origin-z', rowAmount - y);
     });
   }
 }
