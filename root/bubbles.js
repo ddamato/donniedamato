caa89cf8383984bf4429761abdb36435efa512fc
Donnie D'Amato <fauxserious@gmail.com>
2020-08-26 23:59:32 -0400

    Bubble refactor

--- root/bubbles.js
+++ root/bubbles.js
@@ -1,60 +1,59 @@
-const canvas = document.querySelector(".bubbles");
-const ctx = canvas.getContext("2d");
+const canvas = document.querySelector('.bubbles');
+const ctx = canvas.getContext('2d');
 const pieces = [];
 const numberOfPieces = 4;
 let lastUpdateTime = Date.now();
 
-function update() {
-  const now = Date.now();
-  pieces.forEach((p) => {
-    p.y -= p.gravity * (now - lastUpdateTime);
-    if (p.y < -p.dim) p.reset();
-  });
-
-	lastUpdateTime = now;
-	requestAnimationFrame(update);
-}
-
 function draw() {
-  
-  canvas.width = canvas.clientWidth;
-  canvas.height = canvas.clientHeight;
 
-  pieces.forEach((p) => ctx.clearRect(p.x, p.y, p.dim, p.dim));
+  if (canvas.width !== canvas.clientWidth) {
+    canvas.width = canvas.clientWidth;
+  }
 
-	pieces.forEach((p) => {
-		ctx.fillStyle = "#000";
-		ctx.beginPath();
-    ctx.arc(p.x, p.y, p.dim / 2, 0, Math.PI * 2);
-    ctx.fill();
-	});
+  if (canvas.height !== canvas.clientHeight) {
+    canvas.height = canvas.clientHeight;
+  }
 
+  const now = Date.now();
+  pieces.forEach((p) => p.float(now - lastUpdateTime));
+  pieces.forEach((p) => p.draw());
+  lastUpdateTime = now;
 	requestAnimationFrame(draw);
 }
 
 function Piece(x, y) {
   const min = 20;
   const max = 200;
+  const offset = 2;
+
 	this.x = x;
   this.y = y;
+
+  this.setGravity = () => (this.gravity = (Math.random() * 0.5 + 0.75) * 0.2);
+  this.setDim = () => (this.dim = Math.floor(Math.random() * (max - min + 1) + min));
   this.setPosition = ({ x, y }) => {
     this.x = isNaN(x) ? Math.random() * canvas.width : x;
     this.y = isNaN(y) ? Math.random() * canvas.height : y;
-  };
-  this.setGravity = () => (this.gravity = (Math.random() * 0.5 + 0.75) * 0.2);
-  this.setDim = () => (this.dim = Math.floor(Math.random() * (max - min + 1) + min));
-  this.reset = () => {
-    this.setPosition({ y: canvas.height });
     this.setGravity();
     this.setDim();
+  };
+
+  this.draw = () => {
+    ctx.fillStyle = "#000";
+    ctx.beginPath();
+    ctx.arc(this.x + this.dim/2, this.y + this.dim/2, this.dim / 2, 0, Math.PI * 2);
+    ctx.fill();
+  }
+
+  this.float = (amount) => {
+    ctx.clearRect(this.x - offset, this.y - offset, this.dim + offset * 2, this.dim + offset * 2);
+    this.y -= this.gravity * amount;
+    if (this.y < -this.dim) this.setPosition({ y: canvas.height });
   }
 
   this.setPosition({ x, y });
-  this.setGravity();
-  this.setDim();
 }
 
 while (pieces.length < numberOfPieces) pieces.push(new Piece());
 
-update();
 draw();
